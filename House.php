8b1ab82c19efb80f8862965a5185616a4cfafc17
ignaszienius <30698624+ignaszienius@users.noreply.github.com>
2017-09-25 21:23:16 +0300

    Pakurti objektai, ryt reiks pavaryt

--- House.php
+++ House.php
@@ -1,6 +1,6 @@
 <?php
 
-class dog {
+class house {
 	public $owner;
 	public $name;
 	public $size;
--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+echo "<hr>";
+echo "Car";
+echo "<hr>";
+echo "<br>";
 include "Car.php";
 
 $ignomashke = new Car("DIE 666", "Ignas");
@@ -18,3 +23,26 @@ $ignomashke->repair();
 
 echo $ignomashke->license;
 echo $ignomashke->owner;
+
+echo "<hr>";
+echo "Dog";
+echo "<hr>";
+
+include "Dog.php";
+
+$ignodog = new Dog("Ignas", "Killa");
+$ignodog->size = "small";
+$ignodog->color = "black";
+$ignodog->breed = "German Shepherd";
+$ignodog->sex = "male";
+
+
+
+
+
+
+
+
+echo "<hr>";
+echo "House";
+echo "<hr>";
